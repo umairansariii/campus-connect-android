18842c4103465cdbdde23dc8fb5ffe209bf70991
umairansariii <umairansari.work@gmail.com>
2025-03-04 23:54:49 +0500

    feat: add get user query

--- app/src/main/java/com/umairansariii/campusconnect/data/local/dao/UserDao.kt
+++ app/src/main/java/com/umairansariii/campusconnect/data/local/dao/UserDao.kt
@@ -2,14 +2,19 @@ package com.umairansariii.campusconnect.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Delete
+import androidx.room.Query
 import androidx.room.Upsert
-import com.umairansariii.campusconnect.data.local.entities.UserEntity
+import com.umairansariii.campusconnect.data.local.entities.User
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UserDao {
     @Upsert
-    suspend fun upsertUser(user: UserEntity)
+    suspend fun upsertUser(user: User)
 
     @Delete
-    suspend fun deleteUser(user: UserEntity)
+    suspend fun deleteUser(user: User)
+
+    @Query("SELECT * FROM user ORDER BY firstName ASC")
+    fun getUsers(): Flow<List<User>>
 }
