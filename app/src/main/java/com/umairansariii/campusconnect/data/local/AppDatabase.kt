8649f3a50290ebb88dbc728cf27491e9729e8b5d
umairansariii <umairansari.work@gmail.com>
2025-03-05 15:37:08 +0500

    feat: implement database initializer

--- app/src/main/java/com/umairansariii/campusconnect/data/local/AppDatabase.kt
+++ app/src/main/java/com/umairansariii/campusconnect/data/local/AppDatabase.kt
@@ -1,14 +1,38 @@
 package com.umairansariii.campusconnect.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.umairansariii.campusconnect.data.local.converters.DateConverter
 import com.umairansariii.campusconnect.data.local.dao.UserDao
 import com.umairansariii.campusconnect.data.local.entities.User
 
+@TypeConverters(value = [DateConverter::class])
 @Database(
     entities = [User::class],
-    version = 1
+    version = 1,
+    exportSchema = false,
 )
 abstract class AppDatabase: RoomDatabase() {
-    abstract val userDao: UserDao
+    abstract fun userDao(): UserDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context,
+                    AppDatabase::class.java,
+                    name = "campusconnect_db"
+                ).build()
+                INSTANCE = instance
+
+                return instance
+            }
+        }
+    }
 }
