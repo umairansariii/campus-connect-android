de52d19bb7a315b7d2dd423fda3f9d35388d6bb1
umairansariii <umairansari.work@gmail.com>
2025-03-13 13:12:05 +0500

    feat: add student approve dialog component

--- app/src/main/java/com/umairansariii/campusconnect/presentation/screens/StudentScreen.kt
+++ app/src/main/java/com/umairansariii/campusconnect/presentation/screens/StudentScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.umairansariii.campusconnect.presentation.components.StudentCard
+import com.umairansariii.campusconnect.presentation.dialogs.StudentApproveDialog
 import com.umairansariii.campusconnect.presentation.dialogs.StudentUpdateDialog
 import com.umairansariii.campusconnect.presentation.dialogs.StudentViewDialog
 
@@ -48,5 +49,6 @@ fun StudentScreen() {
         }
         StudentViewDialog()
         StudentUpdateDialog()
+        StudentApproveDialog()
     }
 }
