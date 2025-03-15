6ba8083884cc919d8736480125509863652cd375
umairansariii <umairansari.work@gmail.com>
2025-03-15 22:41:24 +0500

    feat: add enrollment entity and dao

--- app/src/main/java/com/umairansariii/campusconnect/data/local/AppDatabase.kt
+++ app/src/main/java/com/umairansariii/campusconnect/data/local/AppDatabase.kt
@@ -6,10 +6,12 @@ import androidx.room.TypeConverters
 import com.umairansariii.campusconnect.data.local.converters.DateConverter
 import com.umairansariii.campusconnect.data.local.dao.CampusDao
 import com.umairansariii.campusconnect.data.local.dao.DepartmentDao
+import com.umairansariii.campusconnect.data.local.dao.EnrollmentDao
 import com.umairansariii.campusconnect.data.local.dao.UniversityDao
 import com.umairansariii.campusconnect.data.local.dao.UserDao
 import com.umairansariii.campusconnect.data.local.entities.Campus
 import com.umairansariii.campusconnect.data.local.entities.Department
+import com.umairansariii.campusconnect.data.local.entities.Enrollment
 import com.umairansariii.campusconnect.data.local.entities.University
 import com.umairansariii.campusconnect.data.local.entities.User
 
@@ -17,6 +19,7 @@ import com.umairansariii.campusconnect.data.local.entities.User
 @Database(
     entities = [
         User::class,
+        Enrollment::class,
         University::class,
         Department::class,
         Campus::class,
@@ -26,6 +29,7 @@ import com.umairansariii.campusconnect.data.local.entities.User
 )
 abstract class AppDatabase: RoomDatabase() {
     abstract fun userDao(): UserDao
+    abstract fun enrollmentDao(): EnrollmentDao
     abstract fun universityDao(): UniversityDao
     abstract fun departmentDao(): DepartmentDao
     abstract fun campusDao(): CampusDao
