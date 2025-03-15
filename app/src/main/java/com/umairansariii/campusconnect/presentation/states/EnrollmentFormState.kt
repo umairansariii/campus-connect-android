e1a7d5ec49c4ff09fde8a4cbe3c2efe940660bed
umairansariii <umairansari.work@gmail.com>
2025-03-15 22:44:14 +0500

    feat: add enrollment state and events

---
+++ app/src/main/java/com/umairansariii/campusconnect/presentation/states/EnrollmentFormState.kt
@@ -0,0 +1,20 @@
+package com.umairansariii.campusconnect.presentation.states
+
+import com.umairansariii.campusconnect.data.local.enums.UserGender
+import java.util.Date
+
+data class EnrollmentFormState (
+    val studentId: Int? = 0,
+    val universityId: Int? = null,
+    val universityIdError: String? = null,
+    val campusId: Int? = null,
+    val campusIdError: String? = null,
+    val departmentId: Int? = null,
+    val departmentIdError: String? = null,
+    val rollNo: String = "",
+    val rollNoError: String? = null,
+    val dob: Date? = null,
+    val dobError: String? = null,
+    val gender: UserGender? = null,
+    val genderError: String? = null,
+)
