79bfc874caf2e64f7f144d993bb4a561d35db131
umairansariii <umairansari.work@gmail.com>
2025-03-16 18:16:02 +0500

    feat: add cards to home screen

--- app/src/main/java/com/umairansariii/campusconnect/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/umairansariii/campusconnect/presentation/screens/HomeScreen.kt
@@ -1,9 +1,103 @@
 package com.umairansariii.campusconnect.presentation.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 
 @Composable
-fun HomeScreen() {
-    Text(text = "Home Screen")
+fun HomeScreen(navController: NavController) {
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background,
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize().padding(horizontal = 8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Card(
+                modifier = Modifier.fillMaxWidth(),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceContainer,
+                ),
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                ) {
+                    Text(
+                        text = "Manage University",
+                        fontSize = 20.sp,
+                    )
+                    Text(
+                        text = "Only admin can manage universities, this feature is temporarily available for testing.",
+                        color = MaterialTheme.colorScheme.secondary,
+                    )
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End,
+                    ) {
+                        Button(
+                            onClick = {
+                                navController.navigate("university")
+                            }
+                        ) {
+                            Text(text = "Manage")
+                        }
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Card(
+                modifier = Modifier.fillMaxWidth(),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceContainer,
+                ),
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                ) {
+                    Text(
+                        text = "Manage Students",
+                        fontSize = 20.sp,
+                    )
+                    Text(
+                        text = "Only admin can manage students, this feature is temporarily available for testing.",
+                        color = MaterialTheme.colorScheme.secondary,
+                    )
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End,
+                    ) {
+                        Button(
+                            onClick = {
+                                navController.navigate("student")
+                            }
+                        ) {
+                            Text(text = "Manage")
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
