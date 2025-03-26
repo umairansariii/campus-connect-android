e7d9487e9465066dcfa7a3050aa5ce6f1a6a2f81
umairansariii <umairansari.work@gmail.com>
2025-03-26 15:46:32 +0500

    feat: implement auth guard in app routes

--- app/src/main/java/com/umairansariii/campusconnect/AppNavigation.kt
+++ app/src/main/java/com/umairansariii/campusconnect/AppNavigation.kt
@@ -1,10 +1,22 @@
 package com.umairansariii.campusconnect
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -23,10 +35,89 @@ import com.umairansariii.campusconnect.presentation.screens.RegisterScreen
 import com.umairansariii.campusconnect.presentation.screens.StudentScreen
 import com.umairansariii.campusconnect.presentation.screens.UniversityDetailScreen
 import com.umairansariii.campusconnect.presentation.screens.UniversityScreen
+import com.umairansariii.campusconnect.viewmodel.AuthViewModel
+import kotlinx.coroutines.flow.collectLatest
+
+fun NavGraphBuilder.authGraph(navController: NavHostController) {
+    navigation(
+        route = "auth", startDestination = "register"
+    ) {
+        composable(route = "login") {
+            LoginScreen(navController = navController)
+        }
+        composable(route = "register") {
+            RegisterScreen(navController = navController)
+        }
+        composable(
+            route = "enrollment/{userId}",
+            arguments = listOf(
+                navArgument(name = "userId") { type = NavType.LongType }
+            )
+        ) { backStackEntry ->
+            val userId = backStackEntry.arguments?.getLong("userId") ?: -1L
+
+            EnrollmentScreen(userId = userId, navController = navController)
+        }
+    }
+}
+
+fun NavGraphBuilder.appGraph(navController: NavHostController) {
+    navigation(
+        route = "app", startDestination = "home"
+    ) {
+        composable(route = "home") {
+            HomeScreen(navController = navController)
+        }
+        composable(route = "university") {
+            UniversityScreen(navController = navController)
+        }
+        composable(
+            route = "university-detail/{universityId}",
+            arguments = listOf(
+                navArgument(name = "universityId") { type = NavType.IntType }
+            )
+        ) { backStackEntry ->
+            val universityId = backStackEntry.arguments?.getInt("universityId") ?: -1
+
+            UniversityDetailScreen(universityId = universityId, navController = navController)
+        }
+        composable(
+            route = "campus/{universityId}",
+            arguments = listOf(
+                navArgument(name = "universityId") { type = NavType.IntType }
+            )
+        ) { backStackEntry ->
+            val universityId = backStackEntry.arguments?.getInt("universityId") ?: -1
+
+            CampusScreen(universityId = universityId)
+        }
+        composable(
+            route = "department/{universityId}",
+            arguments = listOf(
+                navArgument(name = "universityId") { type = NavType.IntType }
+            )
+        ) { backStackEntry ->
+            val universityId = backStackEntry.arguments?.getInt("universityId") ?: -1
+
+            DepartmentScreen(universityId = universityId)
+        }
+        composable(
+            route = "student/{universityId}",
+            arguments = listOf(
+                navArgument(name = "universityId") { type = NavType.IntType }
+            )
+        ) { backStackEntry ->
+            val universityId = backStackEntry.arguments?.getInt("universityId") ?: -1
+
+            StudentScreen(universityId = universityId)
+        }
+    }
+}
 
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
+    val authViewModel: AuthViewModel = hiltViewModel()
 
     Scaffold(
         topBar = {
@@ -42,79 +133,39 @@ fun AppNavigation() {
     ) { innerPadding ->
         NavHost(
             navController = navController,
-            startDestination = "app",
+            startDestination = "loading",
             modifier = Modifier.padding(innerPadding)
         ) {
-            navigation(
-                route = "auth", startDestination = "register"
-            ) {
-                composable(route = "login") {
-                    LoginScreen(navController = navController)
+            composable(route = "loading") {
+                LaunchedEffect(true) {
+                    authViewModel.authState.collectLatest { state ->
+                        if (state.isAuthenticated) {
+                            navController.navigate("app") {
+                                popUpTo(route = "loading") { inclusive = true }
+                            }
+                        }
+                        if (!state.isAuthenticated) {
+                            navController.navigate("auth") {
+                                popUpTo(route = "loading") { inclusive = true }
+                            }
+                        }
+                    }
                 }
-                composable(route = "register") {
-                    RegisterScreen(navController = navController)
-                }
-                composable(
-                    route = "enrollment/{userId}",
-                    arguments = listOf(
-                        navArgument(name = "userId") { type = NavType.LongType }
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.width(64.dp),
+                        color = MaterialTheme.colorScheme.secondary,
+                        trackColor = MaterialTheme.colorScheme.surfaceVariant,
                     )
-                ) { backStackEntry ->
-                    val userId = backStackEntry.arguments?.getLong("userId") ?: -1L
-
-                    EnrollmentScreen(userId = userId, navController = navController)
                 }
             }
-            navigation(
-                route = "app", startDestination = "home"
-            ) {
-                composable(route = "home") {
-                    HomeScreen(navController = navController)
-                }
-                composable(route = "university") {
-                    UniversityScreen(navController = navController)
-                }
-                composable(
-                    route = "university-detail/{universityId}",
-                    arguments = listOf(
-                        navArgument(name = "universityId") { type = NavType.IntType }
-                    )
-                ) { backStackEntry ->
-                    val universityId = backStackEntry.arguments?.getInt("universityId") ?: -1
 
-                    UniversityDetailScreen(universityId = universityId, navController = navController)
-                }
-                composable(
-                    route = "campus/{universityId}",
-                    arguments = listOf(
-                        navArgument(name = "universityId") { type = NavType.IntType }
-                    )
-                ) { backStackEntry ->
-                    val universityId = backStackEntry.arguments?.getInt("universityId") ?: -1
-
-                    CampusScreen(universityId = universityId)
-                }
-                composable(
-                    route = "department/{universityId}",
-                    arguments = listOf(
-                        navArgument(name = "universityId") { type = NavType.IntType }
-                    )
-                ) { backStackEntry ->
-                    val universityId = backStackEntry.arguments?.getInt("universityId") ?: -1
-
-                    DepartmentScreen(universityId = universityId)
-                }
-                composable(
-                    route = "student/{universityId}",
-                    arguments = listOf(
-                        navArgument(name = "universityId") { type = NavType.IntType }
-                    )
-                ) { backStackEntry ->
-                    val universityId = backStackEntry.arguments?.getInt("universityId") ?: -1
-
-                    StudentScreen(universityId = universityId)
-                }
-            }
+            authGraph(navController = navController)
+            appGraph(navController = navController)
         }
     }
 }
