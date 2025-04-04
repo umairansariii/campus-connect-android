b887dd710283dbc9bc49454f2f1fd77b370306b8
umairansariii <umairansari.work@gmail.com>
2025-04-04 18:14:03 +0500

    fix: manage university by admin id

--- app/src/main/java/com/umairansariii/campusconnect/presentation/events/UniversityFormEvent.kt
+++ app/src/main/java/com/umairansariii/campusconnect/presentation/events/UniversityFormEvent.kt
@@ -4,5 +4,5 @@ sealed interface UniversityFormEvent {
     data class ShowDialog(val id: Int?): UniversityFormEvent
     class DismissDialog: UniversityFormEvent
     data class UniversityTitleChanged(val universityTitle: String): UniversityFormEvent
-    data class Submit(val adminId: Int): UniversityFormEvent
+    data class Submit(val adminId: Int?): UniversityFormEvent
 }
