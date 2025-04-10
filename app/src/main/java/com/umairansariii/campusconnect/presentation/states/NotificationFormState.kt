74932c350eb2f77ed5c12918e7dc7d7d53ec85cf
umairansariii <umairansari.work@gmail.com>
2025-04-10 23:51:09 +0500

    feat: add notification state and events

---
+++ app/src/main/java/com/umairansariii/campusconnect/presentation/states/NotificationFormState.kt
@@ -0,0 +1,16 @@
+package com.umairansariii.campusconnect.presentation.states
+
+import java.util.Date
+
+data class NotificationFormState (
+    val showViewDialog: Boolean = false,
+    val showViewDialogId: Int? = null,
+    val showUpdateDialog: Boolean = false,
+    val showUpdateDialogId: Int? = null,
+    // Notification Data
+    val createdAt: Date? = null,
+    val notificationTitle: String = "",
+    val notificationTitleError: String? = null,
+    val notificationDescription: String = "",
+    val notificationDescriptionError: String? = null,
+)
