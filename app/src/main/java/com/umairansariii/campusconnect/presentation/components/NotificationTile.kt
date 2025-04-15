f023e55f9a65376aa7a261dac34dc224c93c8a7f
umairansariii <umairansari.work@gmail.com>
2025-04-16 00:36:28 +0500

    feat: add bottom padding

--- app/src/main/java/com/umairansariii/campusconnect/presentation/components/NotificationTile.kt
+++ app/src/main/java/com/umairansariii/campusconnect/presentation/components/NotificationTile.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -60,4 +62,5 @@ fun NotificationTile(notification: Notification) {
             }
         }
     }
+    Spacer(modifier = Modifier.height(10.dp))
 }
