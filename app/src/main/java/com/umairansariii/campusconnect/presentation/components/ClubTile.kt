856e29eac0825663e3b56a50c33e847663664e09
umairansariii <umairansari.work@gmail.com>
2025-04-18 00:38:21 +0500

    feat: add club tile component

---
+++ app/src/main/java/com/umairansariii/campusconnect/presentation/components/ClubTile.kt
@@ -0,0 +1,55 @@
+package com.umairansariii.campusconnect.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.umairansariii.campusconnect.R
+
+@Composable
+fun ClubTile() {
+    Card(
+        modifier = Modifier.fillMaxWidth().clickable(
+            onClick = { /* Handle click */ }
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceContainer,
+        ),
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(10.dp),
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.profile),
+                contentDescription = "club-tile-banner-image",
+                modifier = Modifier
+                    .height(130.dp)
+                    .width(130.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.FillBounds,
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "Trail Blazers")
+            Text(
+                text = "Hiking/Outdoor",
+                style = MaterialTheme.typography.bodySmall,
+            )
+        }
+    }
+    Spacer(modifier = Modifier.width(10.dp))
+}
