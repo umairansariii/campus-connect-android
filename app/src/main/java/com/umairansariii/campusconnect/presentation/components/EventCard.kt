6e1429c3cec414c8b6240433a3879e268249ff58
umairansariii <umairansari.work@gmail.com>
2025-04-20 20:28:02 +0500

    feat: add event card component

---
+++ app/src/main/java/com/umairansariii/campusconnect/presentation/components/EventCard.kt
@@ -0,0 +1,85 @@
+package com.umairansariii.campusconnect.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.umairansariii.campusconnect.R
+
+@Composable
+fun EventCard() {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceContainer,
+        ),
+    ) {
+        Column {
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 12.dp, horizontal = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.image_placeholder),
+                    contentDescription = "event-card-university-icon",
+                    modifier = Modifier.size(50.dp).clip(CircleShape),
+                    contentScale = ContentScale.Crop,
+                )
+                Column {
+                    Text(text = "Virtual University")
+                    Text(
+                        text = "North Nazimabad Campus",
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+                }
+            }
+            Image(
+                painter = painterResource(id = R.drawable.image_placeholder),
+                contentDescription = "event-card-banner-image",
+                modifier = Modifier.fillMaxWidth().height(200.dp),
+                contentScale = ContentScale.Crop,
+            )
+            Column(
+                modifier = Modifier.fillMaxWidth().padding(16.dp)
+            ) {
+                Text(text = "Planting for Tomorrow: Earth Day 2025")
+                Text(text = "Bagh-e-Jinnah, Karachi.")
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = "Join us in celebrating Earth Day by planting trees and flowers around campus! Help beautify our surroundings and contribute to a greener future.")
+                Spacer(modifier = Modifier.height(10.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End,
+                ) {
+                    Button(
+                        onClick = { /* Handle click */ }
+                    ) {
+                        Text(text = "View Details")
+                    }
+                }
+            }
+        }
+    }
+    Spacer(modifier = Modifier.height(10.dp))
+}
