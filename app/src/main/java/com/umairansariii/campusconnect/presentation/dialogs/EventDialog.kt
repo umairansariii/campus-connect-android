b2ca4af5cc144b1d3ae81aa2d28c8065386ad673
umairansariii <umairansari.work@gmail.com>
2025-04-22 20:28:43 +0500

    feat: add button to event dialog

--- app/src/main/java/com/umairansariii/campusconnect/presentation/dialogs/EventDialog.kt
+++ app/src/main/java/com/umairansariii/campusconnect/presentation/dialogs/EventDialog.kt
@@ -2,14 +2,19 @@ package com.umairansariii.campusconnect.presentation.dialogs
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -61,6 +66,19 @@ fun EventDialog(universityId: Int) {
                         )
                     }
                     Column {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            Text(text = "Marked as active?")
+                            Switch(
+                                checked = state.eventIsActive,
+                                onCheckedChange = {
+                                    viewModel.onEvent(EventFormEvent.EventStateChanged(it))
+                                },
+                            )
+                        }
                         OutlinedTextField(
                             value = state.eventTitle,
                             onValueChange = {
@@ -128,6 +146,22 @@ fun EventDialog(universityId: Int) {
                             supportingText = state.eventTypeError,
                             isError = state.eventTypeError != null,
                         )
+                    }
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                    ) {
+                        Button(
+                            onClick = {
+                                viewModel.onEvent(EventFormEvent.Submit(universityId))
+                            },
+                            modifier = Modifier.fillMaxWidth().height(50.dp)
+                        ) {
+                            if (state.showDialogId !== null) {
+                                Text(text = "Update", style = MaterialTheme.typography.titleMedium)
+                            } else {
+                                Text(text = "Create", style = MaterialTheme.typography.titleMedium)
+                            }
+                        }
                     }
                 }
             }
