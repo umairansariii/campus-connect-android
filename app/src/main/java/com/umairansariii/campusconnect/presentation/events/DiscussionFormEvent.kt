112206575046fcde53f1a598a7dd877224f7e3f7
umairansariii <umairansari.work@gmail.com>
2025-05-05 19:59:36 +0500

    fix: discussion form event parameters

--- app/src/main/java/com/umairansariii/campusconnect/presentation/events/DiscussionFormEvent.kt
+++ app/src/main/java/com/umairansariii/campusconnect/presentation/events/DiscussionFormEvent.kt
@@ -3,9 +3,9 @@ package com.umairansariii.campusconnect.presentation.events
 sealed interface DiscussionFormEvent {
     data class ShowDialog(val id: Int?): DiscussionFormEvent
     class DismissDialog: DiscussionFormEvent
-    data class DiscussionTitleChanged(val clubTitle: String): DiscussionFormEvent
-    data class DiscussionDescriptionChanged(val clubDescription: String): DiscussionFormEvent
-    data class DiscussionStateChanged(val clubIsActive: Boolean): DiscussionFormEvent
-    data class DiscussionQueryChanged(val clubQuery: String): DiscussionFormEvent
+    data class DiscussionTitleChanged(val discussionTitle: String): DiscussionFormEvent
+    data class DiscussionDescriptionChanged(val discussionDescription: String): DiscussionFormEvent
+    data class DiscussionStateChanged(val discussionIsActive: Boolean): DiscussionFormEvent
+    data class DiscussionQueryChanged(val discussionQuery: String): DiscussionFormEvent
     data class Submit(val universityId: Int): DiscussionFormEvent
 }
